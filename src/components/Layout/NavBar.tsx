00fc5fed23549b7d9fc28fc9986053c4d2ee3f52
Ye Myat Hein <yemyathein67@gmail.com>
2024-09-15 02:42:58 +0630

    fix

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { MdMenu } from "react-icons/md";
 import Logo from "../../assets/logo.png";
