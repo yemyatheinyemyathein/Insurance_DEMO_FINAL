c0b21b0c2f68860db0ed9fbdc0f3ffbdb46437bc
Ye Myat Hein <yemyathein67@gmail.com>
2024-09-24 16:09:50 +0630

    fix: STE Main Table

--- src/components/Home/STEMainTable.tsx
+++ src/components/Home/STEMainTable.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const STEMainTable = () => {
   return (
     <div className='mx-auto text-lg text-center my-12 font-bold text-blue-500'>STE MainTable is Under Maintenance</div>
